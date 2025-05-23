1ad0859519e171c61846e18cbffb387dbe2e89ed
Bape <154688709+BapeCode@users.noreply.github.com>
2025-05-23 23:36:03 +0200

    Fix favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,10 @@ export const metadata: Metadata = {
   title: "OnCity Soirée Blanche Reservation",
   description: "Soirée blanche - 17 Juin 2025",
   icons: {
-    icon: "/logo.png",
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/favicon.png", type: "image/png" },
+    ],
   },
   robots: {
     index: false,
