a9e1a6d4571aa5b8e0ab88e5d95b62393d7904f5
Mathieu Forest <154688709+BapeCode@users.noreply.github.com>
2025-05-26 12:55:25 +0200

    Fix footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="flex flex-col md:flex-row items-center justify-center md:justify-between w-full p-4 bg-secondary">
-      <span className="text-white text-md">© 2023 Lyon 6ème & OnCity</span>
+      <span className="text-white text-md">© 2025 Lyon 6ème & OnCity</span>
 
       <Link href={"/cguv"} className="text-white text-sm underline text-center">
         Conditions Général d'utilisation et de vente (CGUV)
